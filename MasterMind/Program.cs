31ba729d7e3889d7b7c8411881f96cd160b598c1
Brandon Haskett <brhaskett@yahoo.com>
2020-03-12 13:27:43 -0400

    creating messages for user

--- MasterMind/Program.cs
+++ MasterMind/Program.cs
@@ -11,15 +11,20 @@ namespace MasterMind
             int tries = 1;
             while (tries <= 10)
             {
-                String input = Console.ReadLine();
+                string input = Console.ReadLine();
                 string result = masterMind.RunCode(input);
                 Console.WriteLine(result);
-                if (result.Length == 4)
+                if(result == "++++")
                 {
+                    Console.WriteLine("Congrats you guessed the code correctly");
+                    break;
+                }
+                else if (result.Length == 4)
+                {
+                    Console.WriteLine("You have {0} tries remaining", 10-tries);
                     tries++;
-                }                
-            }
-            
+                }
+            }            
         }
     }
 }
